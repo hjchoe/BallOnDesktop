0303d1b0d11dee17a828dd9c7262f45650104ec1
James Choe <60300779+hjchoe@users.noreply.github.com>
2021-06-29 19:29:42 -0400

    local data setup edits

--- src/ballondesktop/Setup.java
+++ src/ballondesktop/Setup.java
@@ -1,9 +1,13 @@
 package ballondesktop;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Path;
+import java.util.Scanner;
 
 class Setup
 {
@@ -13,18 +17,23 @@ class Setup
 	
 	public Setup()
 	{
-		//p = Path.of(".").toAbsolutePath();
-		//dataFolder = new File(p + "/DATA");
-		//dataFile = new File(dataFolder + "/data.txt");
+		p = Path.of(".").toAbsolutePath();
+		dataFolder = new File(p + "/DATA");
+		dataFile = new File(dataFolder + "/data.txt");
 		
-		//if (createDataFolder())
+		if (createDataFolder())
 		{
-			//createDataFile();
-			//setupDataFile();
+			createDataFile();
+			setupDataFile();
 		}
-		//else
+		
+		try
 		{
-			
+			updateData("balance", "100");
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
 		}
 	}
 	
@@ -61,7 +70,7 @@ class Setup
 		try
 		{
 			FileWriter writer = new FileWriter(dataFile, false);
-		    writer.write("balance$@#0\nballColor$@#0,255,255\ntrailColor$@#255,0,0\nbackgroundColor$@#0,0,0,1\nballSize$@#");
+		    writer.write("balance$@#0\nballColor$@#0,255,255\ntrailColor$@#255,0,0\nbackgroundColor$@#0,0,0,1\nballSize$@#10");
 		    writer.flush();
 		    writer.close();
 		}
@@ -71,5 +80,47 @@ class Setup
 		    e.printStackTrace();
 		    System.exit(0);
 		}
+	}
+	
+	public String getData(String section) throws FileNotFoundException
+	{
+		String result = "error";
+		
+		Scanner s = new Scanner(dataFile);
+	    while (s.hasNextLine())
+	    {
+	    	String[] data = s.nextLine().replace("\n", "").split("\\$@#");
+	    	if (data[0].equals(section))
+	    	{
+	    		result = data[1];
+	    		break;
+	    	}
+	    }
+	    s.close();
+		
+		return result;
+	}
+	
+	public void updateData(String section, String value) throws IOException
+	{
+		BufferedReader file = new BufferedReader(new FileReader(dataFile));
+		String data = "";
+        String line;
+        
+        while ((line = file.readLine()) != null)
+        {
+        	String temp = line.replace("\n", "");
+        	if (temp.split("\\$@#")[0] == section)
+        	{
+        		line.replace(temp.split("\\$@#")[1], "value");
+        	}
+        	data += (line+"\n");
+        }
+        file.close();
+        
+		FileWriter writer = new FileWriter(dataFile, false);
+	    writer.write(data);
+	    writer.flush();
+	    writer.close();
 	}
 }
